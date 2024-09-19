58a64b58c701e62375b6412e4efdc75914265761
Matheus <matheushenriquevitorino@gmail.com>
2024-09-19 18:34:20 -0300

    Atualização metodo do post da home

--- blog/views.py
+++ blog/views.py
@@ -7,23 +7,19 @@ def home(request):
     #obtém todos posts
     posts = Post.objects.order_by('-data')
 
-    
     # pega o primeiro post
     post_destaque = posts.first()
     
     # exclui o primeiro da fila, e pega os outros
     post_secundario = posts.exclude(id=post_destaque.id)[:2]
-    
 
-    
-    post_inicial = posts[4]
+    post_inicial = Post.objects.all()[3]
 
-    
-    
-    
+        
+  
     return render(
         request,'home.html',
-        {'post_destaque':post_destaque,'post_secundario':post_secundario, 'posts':posts,'post_inicial':post_inicial})
+        {'post_destaque':post_destaque,'post_secundario':post_secundario, 'posts':posts, 'post_inicial':post_inicial})
     
     
 def postar(request):
@@ -38,22 +34,17 @@ def postar(request):
         conteudo = request.POST.get('conteudo')
         tag_id = request.POST.getlist('tag')#getlist chama por lista
         
-        
         categoria_id = int(request.POST.get('categoria', 0))  
         categoria = get_object_or_404(Categoria,id=categoria_id)# Busca a categoria pelo ID 
-        
-        
-        post = Post(autor=autor, titulo = titulo, resumo = resumo,  conteudo = conteudo, data = data, categoria = categoria)
-        
+                
+        post = Post(autor=autor, titulo = titulo, resumo = resumo,  conteudo = conteudo, data = data, categoria = categoria)       
         post.save() 
-
         
         if tag_id:
             for tag_id in tag_id:
                 tag = get_object_or_404(Tag,id=tag_id)
                 post.tag.add(tag)#O método add() é usado para adicionar uma ou mais instâncias ao campo ManyToManyField
 
-        
         return redirect('postar')
         
     return render(request,'postar.html',{ 'categoria_tags': categoria_tags, 'tag_box': tag_box})
